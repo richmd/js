cf28a270369868fbdda8634796c1abcab635f912
bebeji-nappa <masadecade@gmail.com>
2025-03-26 00:42:49 +0900

    blockquote 修正

--- src/convert/blockquote.ts
+++ src/convert/blockquote.ts
@@ -3,49 +3,48 @@ import { changeHtml } from "./changeHtml";
 
 export const blockquote = (values: Convert[][]) => {
   let bq = '<blockquote class="blockquote">\n';
-  for (const key in values) {
+  for (const value of values) {
     let text = '<span class="span">';
-    const data: Convert[] = values[key];
-    for (const val in data) {
-      switch (data[val].name) {
+    for (const data of value) {
+      switch (data.name) {
         case "em":
-          text += `<strong>${data[val].value}</strong>`;
+          text += `<strong>${data.value}</strong>`;
           break;
         case "strikethrough":
-          text += `<del>${data[val].value}</del>`;
+          text += `<del>${data.value}</del>`;
           break;
         case "italic":
-          text += `<em>${data[val].value}</em>`;
+          text += `<em>${data.value}</em>`;
           break;
         case "emitalic":
-          text += `<em><strong>${data[val].value}</strong></em>`;
+          text += `<em><strong>${data.value}</strong></em>`;
           break;
         case "link": {
-          const path = changeHtml(data[val].href);
-          text += `<a href="${path}" class="a">${data[val].title}</a>`;
+          const path = changeHtml(data.href);
+          text += `<a href="${path}" class="a">${data.title}</a>`;
           break;
         }
         case "image":
-          text += `<img src="${data[val].src}" alt="${data[val].alt}" />`;
+          text += `<img src="${data.src}" alt="${data.alt}" />`;
           break;
         case "video":
-          text += `<video controls preload="none" class="video">\n<source src="${data[val].src}" />\nSorry, your browser doesn't support embedded videos.\n</video>`;
+          text += `<video controls preload="none" class="video">\n<source src="${data.src}" />\nSorry, your browser doesn't support embedded videos.\n</video>`;
           break;
         case "code":
-          text += `<code class="inline-code">${data[val].value}</code>`;
+          text += `<code class="inline-code">${data.value}</code>`;
           break;
         case "katex": {
-          const html = Katex.renderToString(String.raw`\displaystyle ${data[val].value}`, {
+          const html = Katex.renderToString(String.raw`\displaystyle ${data.value}`, {
             throwOnError: false,
           });
           text += html;
           break;
         }
         default: {
-          if (data[val].value.match(/\n/)) {
-            text += data[val].value.replace(/\n/g, "<br>");
+          if (data.value.match(/\n/)) {
+            text += data.value.replace(/\n/g, "<br>");
           } else {
-            text += data[val].value + "<br>";
+            text += data.value + "<br>";
           }
           break;
         }
