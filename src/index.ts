93115e9d883925f6f6da75d4f3ab24b15a3370c2
bebeji-nappa <masadecade@gmail.com>
2025-10-08 01:56:04 +0900

    絵文字に対応

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { convertMdTree } from "./convertMdTree";
 import "./type";
 import "../styles/richmd.css";
 
-export const richmd = (text: string) => convertMdTree(parseTree(text));
+export const richmd = (text: string, useSlide: boolean = true) => convertMdTree(parseTree(text, useSlide));
 
 // @ts-ignore-next-line
 export const convertTree = (data: any) => convertMdTree(data);
